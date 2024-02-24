6023400cd9d109e4793701a4ed6251e3ee5dd2a0
aaronhyzhang <aaron11886z@gmail.com>
2024-02-24 01:10:27 -0600

    revert

---
+++ main/server.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./backend/middleware/errorMiddleware");
+const connectDB = require("./backend/config/db");
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./backend/routes/userRoutes"));
+
+// Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
